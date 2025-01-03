4094fd24c189e76a2050dd43d419e607688c11af
adam12a12 <adam12a12@gmail.com>
2025-01-03 13:53:00 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,175 @@
+import logging
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import cv2
+import time
+import torch
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.resnet50 import preprocess_input
+import requests
+import smtplib
+from twilio.rest import Client
+from email.mime.text import MIMEText
+import os
+from ultralytics import YOLO
+from dotenv import load_dotenv
+from PIL import Image
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+load_dotenv()
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
+email_address = os.getenv("EMAIL_ADDRESS")
+email_password = os.getenv("EMAIL_PASSWORD")
+
+CAMERA_URL = "http://172.16.26.49:8080/video"
+
+WEIGHTS = {
+    'yolo': 0.7,
+    'resnet': 0.3
+}
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+IMAGES_FOLDER = BASE_DIR / "pictures"
+
+OUTPUT_FOLDER = BASE_DIR / "output"
+
+yolo_model = YOLO(settings.BASE_DIR / "ai_models/best.pt", task="predict")
+
+RESNET_MODEL_PATH = settings.BASE_DIR / "ai_models/ResNet50/resnet50_model.h5"
+resnet_model = load_model(RESNET_MODEL_PATH)
+
+VIDEO_STREAM = cv2.VideoCapture(CAMERA_URL)
+
+if not VIDEO_STREAM.isOpened():
+    logger.error("Unable to connect to CV")
+else:
+    logger.info("CV connected")
+
+def preprocess_image(image):
+    image = cv2.resize(image, (224, 224))
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = preprocess_input(image)
+    return image
+
+def weighted_voting(yolo_result, resnet_result):
+    weighted_sum = (yolo_result * WEIGHTS['yolo'] + resnet_result * WEIGHTS['resnet'])
+    return 1 if weighted_sum > 0.5 else 0
+
+def send_whatsapp_alert(message):
+    client = Client(account_sid, auth_token)
+    from_whatsapp_number = 'whatsapp:+14155238886'
+    to_whatsapp_number = 'whatsapp:+966561227480'
+    try:
+        msg = client.messages.create(body=message, from_=from_whatsapp_number, to=to_whatsapp_number)
+        logger.info(f"WhatsApp message sent: {msg.sid}")
+    except Exception as e:
+        logger.error(f"Error sending WhatsApp message: {e}")
+
+def send_telegram_alert(message):
+    url = f'https://api.telegram.org/bot{telegram_bot_token}/sendMessage'
+    params = {'chat_id': telegram_chat_id, 'text': message}
+    try:
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            logger.info("Telegram message sent")
+        else:
+            logger.warning(f"فشل في إرسال تليجرام. الكود: {response.status_code}")
+    except Exception as e:
+        logger.error(f"Telegram error: {e}")
+
+def send_email_alert(subject, message, to_email):
+    msg = MIMEText(message)
+    msg["Subject"] = subject
+    msg["From"] = email_address
+    msg["To"] = to_email
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(email_address, email_password)
+            server.sendmail(email_address, to_email, msg.as_string())
+        logger.info("Email sent successfully")
+    except Exception as e:
+        logger.error(f"Error sending email: {e}")
+
+def send_alert(message):
+    send_whatsapp_alert(message)
+    send_telegram_alert(message)
+    send_email_alert("تنبيه خطر", message, email_address)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def get_video_frame():
+    global VIDEO_STREAM
+    if not VIDEO_STREAM.isOpened():
+        logger.error("Unable to open video with OpenCV")
+        return None
+
+    ret, frame = VIDEO_STREAM.read()
+    if not ret:
+        logger.error("Error reading video frame")
+        return None
+
+    logger.info("Video frame read successfully")
+    return frame
+
+@socketio.on('connect')
+def handle_connect(sid):
+    start_time = time.time()
+    while True:
+        frame = get_video_frame()
+        if frame is not None:
+            results = yolo_model(frame)
+            detections = results[0].boxes
+            yolo_hazard = 1 if len(detections) > 0 else 0
+            logger.info(f"YOLO results: {yolo_hazard}")
+
+            preprocessed_frame = preprocess_image(frame)
+            resnet_prediction = resnet_model.predict(preprocessed_frame)
+            resnet_hazard = 1 if np.argmax(resnet_prediction) == 1 else 0
+            logger.info(f"ResNet50 results: {resnet_hazard}")
+
+            vote = weighted_voting(yolo_hazard, resnet_hazard)
+            logger.info(f"Weighted vote results: {vote}")
+
+
+            detections_data = results[0].boxes.data.cpu().numpy()
+            yolo_results = [{'class_id': int(det[5]), 'confidence': float(det[4]), 'x_min': float(det[0]), 'y_min': float(det[1]), 'x_max': float(det[2]), 'y_max': float(det[3])} for det in detections_data]
+
+
+            socketio.emit('hazard_update', {
+                'yolo_results': yolo_results,
+                'resnet_results': int(resnet_hazard),
+                'weighted_vote': int(vote)
+            })
+
+
+            if vote == 1:
+                send_alert("خطر اكتُشف في الموقع!")
+
+            if time.time() - start_time >= 10:
+                logger.info("الكود يعمل بشكل صحيح، تم تنفيذ الكشف!")
+                start_time = time.time()
+
+            time.sleep(1)
+        else:
+            logger.error("فشل في الحصول على الإطار من الكاميرا.")
+            break
+
+if __name__ == "__main__":
+    logger.info("Running App")
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
---
+++ static/js/scan.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById('start-btn').onclick = async function() {
+    document.getElementById('loading').style.display = 'block';
+
+    try {
+      const response = await fetch('/start_processing');
+      const data = await response.json();
+      const alertDiv = document.getElementById('alert');
+
+      document.getElementById('loading').style.display = 'none';
+
+      alertDiv.textContent =' تم معالجة ${data.processed_count} صورة تحتوي على مخاطر.';
+
+    } catch (error) {
+      console.error('خطأ أثناء المعالجة:', error);
+      document.getElementById('loading').style.display = 'none';
+      document.getElementById('alert').textContent = 'حدث خطأ أثناء معالجة الصور.';
+    }
+  }
